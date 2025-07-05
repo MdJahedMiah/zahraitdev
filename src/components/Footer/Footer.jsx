734f4295c4f8de8589b19421303153d26fda00ae
Md Jahed Miah <mm22aif@herts.ac.uk>
2025-07-05 14:03:01 +0100

    update in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <h2 className="text-xl font-semibold mb-3">Follow Me</h2>
         <div className="flex justify-center space-x-4 text-lg ">
             <a
-              href="https://www.linkedin.com/in/md-jahed-miah-1601b3211/"
+              href="https://www.linkedin.com/in/zahrait-dev-840667369/"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Md Jahed Miah LinkedIn Profile"
@@ -16,7 +16,7 @@ const Footer = () => {
             </a>
 
             <a
-              href="https://www.facebook.com/mdjahedmiahuk/"
+              href="https://www.facebook.com/profile.php?id=100064773381842"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Md Jahed Miah Facebook Profile"
@@ -25,21 +25,12 @@ const Footer = () => {
             </a>
 
             <a
-              href="https://www.instagram.com/jahed.miah98/"
+              href="https://www.instagram.com/zahrait.dev/"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Md Jahed Miah Instagram Profile"
             >
               <FaInstagram className="text-3xl hover:text-pink-500 transition-colors duration-300" />
-            </a>
-
-            <a
-              href="https://github.com/MdJahedMiah"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Md Jahed Miah GitHub Profile"
-            >
-              <FaGithub className="text-3xl hover:text-green-500 transition-colors duration-300" />
             </a>
         </div>
         <p className="mt-4 text-sm opacity-75 pb-6">&copy; {new Date().getFullYear()} ZahraITDev. All rights reserved.</p>
